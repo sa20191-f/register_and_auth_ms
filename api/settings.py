bfd041ee93c8ad67f2bdeda812b6fd838a738efe
Federico G <federgolo@hotmail.com>
2019-05-19 20:35:38 -0500

    Add dockerfile and docker-compose files

--- api/settings.py
+++ api/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '5t1n01feet(z4$otz-w9xc$y$6hn3qx396-#+0mezi4092t25y'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.99.102']
 
 
 # Application definition
