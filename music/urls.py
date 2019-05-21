f4a6a6f77d74a0ae7e15defa6dc7ed8883d90752
Federico G <federgolo@hotmail.com>
2019-05-21 02:26:34 -0500

    Add Logout view and path

--- music/urls.py
+++ music/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import ListSongsView, LoginView, RegisterUsersView
-
+from .views import ListSongsView, LoginView, LogoutView, RegisterUsersView
 
 urlpatterns = [
     path('songs/', ListSongsView.as_view(), name="songs-all"),
-    path('auth/login/', LoginView.as_view(), name="auth-login"),
-    path('auth/register/', RegisterUsersView.as_view(), name="auth-register"),
+    path('login/', LoginView.as_view(), name="auth-login"),
+    path('logout/', LogoutView.as_view(), name="auth-logout"),
+    path('register/', RegisterUsersView.as_view(), name="auth-register"),
 ]
--- music/views.py
+++ music/views.py
@@ -3,7 +3,7 @@ from rest_framework_jwt.settings import api_settings
 from rest_framework import generics, permissions
 from rest_framework.response import Response
 from rest_framework.views import status
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from .serializers import SongsSerializer, TokenSerializer, UserSerializer
 from .models import Songs
@@ -20,37 +20,6 @@ class ListSongsView(generics.ListAPIView):
     queryset = Songs.objects.all()
     serializer_class = SongsSerializer
     permission_classes = (permissions.IsAuthenticated,)
-
-class LoginView(generics.CreateAPIView):
-    """
-    POST auth/login/
-    """
-    serializer_class = UserSerializer
-    # This permission class will overide the global permission
-    # class setting
-    permission_classes = (permissions.AllowAny,)
-
-    queryset = User.objects.all()
-
-    def post(self, request, *args, **kwargs):
-        username = request.data.get("username")
-        password = request.data.get("password")
-        user = authenticate(request, username=username, password=password)
-
-        if user is not None:
-            # login saves the user’s ID in the session,
-            # using Django’s session framework.
-            login(request, user)
-            serializer = TokenSerializer(data={
-                # using drf jwt utility functions to generate a token
-                "token": jwt_encode_handler(
-                    jwt_payload_handler(user)
-                )})
-            serializer.is_valid()
-            print("s", serializer.is_valid())
-            
-            return Response(serializer.data)
-        return Response(status=status.HTTP_401_UNAUTHORIZED)
 
 class RegisterUsersView(generics.CreateAPIView):
     """
@@ -79,4 +48,40 @@ class RegisterUsersView(generics.CreateAPIView):
             data=UserSerializer(new_user).data,
             status=status.HTTP_201_CREATED
         )
+
+class LoginView(generics.CreateAPIView):
+    """
+    POST auth/login/
+    """
+    serializer_class = UserSerializer
+    # This permission class will overide the global permission
+    # class setting
+    permission_classes = (permissions.AllowAny,)
+
+    queryset = User.objects.all()
+
+    def post(self, request, *args, **kwargs):
+        username = request.data.get("username")
+        password = request.data.get("password")
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            # login saves the user’s ID in the session,
+            # using Django’s session framework.
+            login(request, user)
+            serializer = TokenSerializer(data={
+                # using drf jwt utility functions to generate a token
+                "token": jwt_encode_handler(
+                    jwt_payload_handler(user)
+                )})
+            serializer.is_valid()
+            return Response(serializer.data)
+        return Response(status=status.HTTP_401_UNAUTHORIZED)
+
+class LogoutView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    def get(self, request, *args, **kwargs):
+
+        logout(request)
+        return Response(status=status.HTTP_200_OK)
 
