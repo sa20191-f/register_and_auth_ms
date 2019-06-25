fb0b4bd39e93602c5ea1331b083f6a83a7bb7610
Federico G <federgolo@hotmail.com>
2019-06-25 11:37:47 -0500

    small fix

--- api/settings.py
+++ api/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-import datetimehttps://stackoverflow.com/questions/49903923/django-settings-py-not-updating-on-production
+import datetime
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
